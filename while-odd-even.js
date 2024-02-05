175737106f315fad73d9eb47eb48bcf58034d513
Khadija12319 <khadijaatul15-12319@diu.edu.bd>
2024-02-06 02:00:24 +0600

    two new insertion

---
+++ while-odd-even.js
@@ -0,0 +1,16 @@
+let x= 61;
+let y=78;
+console.log("odd numbers are:");
+while(x<=100){
+    if(x%2 != 0){
+        console.log(x);
+    }
+    x++;
+}
+console.log("Even numbers are:");
+while(y<=98){
+    if(y%2 == 0){
+        console.log(y);
+    }
+    y++;
+}
